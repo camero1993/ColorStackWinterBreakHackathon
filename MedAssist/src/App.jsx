e65e0ba5fea5dfd0692b4f42ef96deeb8bffbcbc
David Juarez <85270302+djuarez2652@users.noreply.github.com>
2025-01-07 01:08:06 -0500

    feat: add sign up/in feature using firebase (#1)

--- MedAssist/src/App.jsx
+++ MedAssist/src/App.jsx
@@ -1,10 +1,24 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Signup from "./components/Signup.jsx";
+import Signin from "./components/Signin.jsx";
 import LandingPage from './components/LandingPage';
+
 function App() {
 
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/signin" element={<Signin />} />
+            </Route>
+        )
+    )
+
   return (
-    <>
-      <LandingPage />
-    </>
+      <>
+          <RouterProvider router={router}/>
+      </>
   )
 }
 
